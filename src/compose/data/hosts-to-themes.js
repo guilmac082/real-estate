bdf90b1eb74516d5e4e7cf5474c2ba7646e865ba
guilmac082 <guilherme.up24@gmail.com>
2022-11-28 12:02:06 +0000

    Use hosts-to-themes file in routes to lookup theme name from hostname

---
+++ src/compose/data/hosts-to-themes.js
@@ -0,0 +1,14 @@
+export const hostsToThemes = {
+  "florida.lvh.me": "florida",
+  "alaska.lvh.me": "alaska",
+  "mega.lvh.me": "mega",
+  "quasar-pwb-2022-nov-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "florida"
+}
+// module.exports = {
+//   hostsToThemes: {
+//     "fsd": "dfsd",
+//     kiev: {
+//       primary: 'red'
+//     }
+//   }
+// }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,23 @@
+// const allColorSchemes = require('./src/compose/data/all-color-schemes.json')
+// import allColorSchemes from 'src/compose/data/all-color-schemes.json' // assert {type: 'json'}
+import { hostsToThemes } from 'src/compose/data/hosts-to-themes'
 
+let themeFrameName = process.env.THEME_NAME
+if (process.env.SERVER) {
+  // console.log(`ssr hostname is ${ssrContext.req.hostname}`)
+  // if (hostsToThemes[window.location.hostname]) {
+  //   themeFrameName = hostsToThemes[window.location.hostname]
+  // }
+}
+else {
+  if (hostsToThemes[window.location.hostname]) {
+    themeFrameName = hostsToThemes[window.location.hostname]
+  }
+}
 const routes = [
   {
     path: '/',
-    component: () => import(/* @vite-ignore */`/src/layouts/${process.env.THEME_NAME}/MainLayout.vue`),
+    component: () => import(/* @vite-ignore */`/src/layouts/${themeFrameName}/MainLayout.vue`),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'landing', component: () => import('pages/LandingPage.vue') }
