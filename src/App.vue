5df517845f7f49e8aa914f0e25134a52fc29aa93
guilmac082 <guilherme.up24@gmail.com>
2022-11-29 12:49:56 +0000

    Set css variables from file based on theme

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,24 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
+import { colors, setCssVar } from "quasar"
+import { themeVariables } from "src/compose/data/theme-variables"
 
 export default defineComponent({
-  name: 'App'
+  name: "App",
+  mounted() {
+    let themeName = process.env.THEME_NAME || "florida"
+    this.setColorScheme(themeName)
+  },
+  methods: {
+    setColorScheme(themeName) {
+      let currentThemeVariables = themeVariables[themeName] || []
+      currentThemeVariables.forEach((tVar) => {
+        setCssVar(tVar["varName"], tVar["varValue"])
+      })
+      // setCssVar("pwb-primary", "blue")
+    },
+  },
 })
 </script>
---
+++ src/compose/data/theme-variables.js
@@ -0,0 +1,25 @@
+export const themeVariables = {
+  florida: [
+    {
+      varName: "pwb-primary",
+      varValue: "blue"
+    },
+    {
+      varName: "pwb-primary-contrast",
+      varValue: "yellow"
+    },
+
+  ],
+  mega: [
+    {
+      varName: "pwb-primary",
+      varValue: "green"
+    },
+    {
+      varName: "pwb-primary-contrast",
+      varValue: "white"
+    },
+
+  ]
+
+}
