45459929a57aab42aa57f7e9aea27639d4a7b6c2
guilmac082 <guilherme.up24@gmail.com>
2022-11-30 20:27:13 +0000

    Move calculation of themeFrameName from router/routes to router/index

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { hostsToThemes } from 'src/compose/data/hosts-to-themes'
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
@@ -12,6 +13,19 @@ import routes from './routes'
  */
 
 export default route(function ({ store, ssrContext }) {
+  let themeFrameName = process.env.THEME_NAME
+  if (process.env.SERVER) {
+    console.log(`ssr hostname is ${ssrContext.req.hostname}`)
+    if (hostsToThemes[ssrContext.req.hostname]) {
+      themeFrameName = hostsToThemes[ssrContext.req.hostname]
+    }
+  }
+  else {
+    if (hostsToThemes[window.location.hostname]) {
+      themeFrameName = hostsToThemes[window.location.hostname]
+    }
+  }
+
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
@@ -22,7 +36,8 @@ export default route(function ({ store, ssrContext }) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     // routes,
-    routes: routes(ssrContext),
+    // routes: routes(ssrContext),
+    routes: routes(themeFrameName),
     // mode: process.env.VUE_ROUTER_MODE,
     // base: process.env.VUE_ROUTER_BASE
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,11 @@
-// const allColorSchemes = require('./src/compose/data/all-color-schemes.json')
-// import allColorSchemes from 'src/compose/data/all-color-schemes.json' // assert {type: 'json'}
-import { hostsToThemes } from 'src/compose/data/hosts-to-themes'
-
 // import { Platform } from 'quasar'
 // const platform = process.env.SERVER ? Platform.parseSSR(ssrContext) : Platform
 
 // const routes = []
 // https://forum.quasar-framework.org/topic/5462/solved-how-to-dynamically-change-layout-in-router-base-on-platform/6
 // Inspiration for below came from above
-const routes = (ssrContext) => {
-  let themeFrameName = process.env.THEME_NAME
-  if (process.env.SERVER) {
-    console.log(`ssr hostname is ${ssrContext.req.hostname}`)
-    if (hostsToThemes[ssrContext.req.hostname]) {
-      themeFrameName = hostsToThemes[ssrContext.req.hostname]
-    }
-  }
-  else {
-    if (hostsToThemes[window.location.hostname]) {
-      themeFrameName = hostsToThemes[window.location.hostname]
-    }
-  }
+const routes = (themeFrameName) => {
+
   return [
     {
       path: '/',
