eee8bdf43deb45693cfc446f4b46545310657dea
guilmac082 <guilherme.up24@gmail.com>
2022-12-06 10:17:01 +0000

    Add extra host to themes mappings

--- src/compose/data/hosts-to-themes.js
+++ src/compose/data/hosts-to-themes.js
@@ -5,6 +5,10 @@ export const hostsToThemes = {
   "quasar-pwb-2022-nov-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "florida",
   "pwb-florida-dec-3-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "florida",
   "pwb-mega-dec-5-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "mega",
+  "florida-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "florida",
+  "alaska-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "alaska",
+  "daytona-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "daytona",
+  "florida-dot-gae-quasar-2022-pwb.ew.r.appspot.com": "florida",
   // https://quasar-real-estate-ssr-dot-gae-quasar-2022-pwb.ew.r.appspot.com/en
 }
 // module.exports = {
