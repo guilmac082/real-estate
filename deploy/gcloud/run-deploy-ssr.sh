b60b1bda6f02d39e48ef61a4cfd75a47435253c9
guilmac082 <guilherme.up24@gmail.com>
2022-12-06 20:45:45 +0000

    Minor improvements to deploy script

--- deploy/gcloud/run-deploy-ssr.sh
+++ deploy/gcloud/run-deploy-ssr.sh
@@ -5,24 +5,25 @@
 # Run this file by first changing directory to "./deploy/gcloud"
 # and then typing "./run-deploy-ssr.sh"
 
-service_name="pwb-mega-dec-5"
-now=$(date +"%Y-%m-%d")
-build_log_file="./services/${service_name}-$now-build.result.txt"
-deploy_log_file="../../deploy/gcloud/services/${service_name}-$now-deploy.result.txt"
-echo "${now}"
+service_name="florida"
+date_now=$(date +"%Y-%m-%d")
+time_now=$(date +"%H-%M")
+build_log_file="./services/${service_name}-$date_now-build.result.txt"
+deploy_log_file="../../deploy/gcloud/services/${service_name}-$date_now-deploy.result.txt"
+echo "${date_now}"
 
 # exec >>$log_file 2>&1 && tail $log_file
 
 # below errors:
-# make 2>&1 | tee file.txt
+# make 2>&1 | tee -a file.txt
 # below just hangs
 # exec &> >(tee -a $log_file)
 # below 2 do not show output on screen:
 # exec >>$log_file 2>&1 && tail $log_file
 # exec &>>$log_file && tail $log_file
 
-# exec 2>&1 | tee $log_file
-# quasar build -m ssr 2>&1 | tee $build_log_file
+# exec 2>&1 | tee -a $log_file
+quasar build -m ssr 2>&1 | tee -a $build_log_file
 
 echo "copying ./services/${service_name}/app-ssr.yaml "
 echo "$PWD"
@@ -31,14 +32,14 @@ if cp -f ./services/${service_name}/app-ssr.yaml ../../dist/ssr; then
   cd "../../dist/ssr"
   touch $deploy_log_file
   echo "cd-ing to ../../dist/ssr"
-  echo "deploying from: "
-  echo "$PWD" | tee $deploy_log_file
-  gcloud app deploy --version=v-1a --appyaml=app-ssr.yaml 2>&1 | tee $deploy_log_file
-
-  echo "Finished deploying" 2>&1 | tee $deploy_log_file
-
-  gcloud app browse -s $service_name
-  gcloud app logs tail -s $service_name
+  echo "deploying from:  $PWD" | tee -a $deploy_log_file
+  if gcloud app deploy --version=v-1a --appyaml=app-ssr.yaml 2>&1 | tee -a $deploy_log_file; then
+    echo "Finished deploying" 2>&1 | tee -a $deploy_log_file
+    gcloud app browse -s $service_name
+    gcloud app logs tail -s $service_name
+  else
+    echo "Unable to deploy to gcloud" 2>&1 | tee -a $deploy_log_file
+  fi
 else
-  echo "Unable to deploy" 2>&1 | tee $deploy_log_file
+  echo "Unable to copy deploy yaml file" 2>&1 | tee -a $deploy_log_file
 fi
